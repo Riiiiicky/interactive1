035a1522aaa1817da2c5b804744bc96b05883db2
Riiiiicky <beizizhang@cca.edu>
2017-03-06 17:25:24 -0800

    1

---
+++ QRCODE/js/code.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#content").hide();
+
+$("#pic").click(function() {
+  $("#content").show();
+  $("#pic").hide();
+  
+ $("#content").click(function() {
+  $("#content").hide();
+  $("#pic").show();
+  
+});          });          });
---
+++ QRCODE/js/codecopy.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#contact-window").hide();
+
+$("#contacteing").click(function() {
+  $("#contact-window").show();
+  
+ $("#xcontact").click(function() {
+  $("#contact-window").hide();
+  
+});          });          });
